440a11c12f2fc4aa046ecc1c5201e75a7536d2c6
libewa <libewa.git@icloud.com>
2024-04-20 11:22:54 +0200

    Initial Commit

---
+++ prime/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  prime
+//
+//  Created by Linus Warnatz on 20.04.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ prime/primeApp.swift
@@ -0,0 +1,17 @@
+//
+//  primeApp.swift
+//  prime
+//
+//  Created by Linus Warnatz on 20.04.24.
+//
+
+import SwiftUI
+
+@main
+struct primeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
